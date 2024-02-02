827a6b4677116b233e715e334e8a0a162fc37657
Robinson D. S. Santos <robinsondssantos@gmail.com>
2024-02-02 11:02:06 -0300

    Phoenix LiveView Tetris in Elixir 10

--- lib/tetris_ui_web/live/tetris_live.ex
+++ lib/tetris_ui_web/live/tetris_live.ex
@@ -1,9 +1,20 @@
 defmodule TetrisUiWeb.TetrisLive do
   use Phoenix.LiveView
 
+  def mount(_params, _session, socket) do
+    {
+      :ok,
+      assign(
+        socket,
+        tetromino: Tetris.Brick.new_random |> Tetris.Brick.to_string
+      )
+    }
+  end
+
   def render(assigns) do
     ~H"""
-    Hello World!
+    <h1>Hello</h1>
+    <pre><%= @tetromino %></pre>
     """
   end
 end
